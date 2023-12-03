ee721f3fe4cb25dded1895408af6f3a3ae439f4b
Mr-Shadow-Mc <gameurminecraft22@gmail.com>
2023-12-03 21:48:18 +0100

    Update

--- js/video_checker.js
+++ js/video_checker.js
@@ -2,12 +2,27 @@ var mainVideo1 = $('#home_video_1');
 var mainVideo2 = $('#home_video_2');
 var mainVideo3 = $('#home_video_3');
 
-if ($(window).width() < 1200) {
-    mainVideo1.append("<source type='video/mp4' src='./assets/videos/home_1_mobile.mp4' />");
-    mainVideo2.append("<source type='video/mp4' src='./assets/videos/home_2_mobile.mp4' />");
-    mainVideo3.append("<source type='video/mp4' src='./assets/videos/home_3_mobile.mp4' />");
-} else {
-  mainVideo1.append("<source type='video/mp4' src='./assets/videos/home_1.mp4' />");
-  mainVideo2.append("<source type='video/mp4' src='./assets/videos/home_2.mp4' />");
-  mainVideo3.append("<source type='video/mp4' src='./assets/videos/home_3.mp4' />");
+$(document).ready(function () {
+  checkScreen();
+});
+
+$(window).on('resize', function () {
+  checkScreen();
+});
+
+function checkScreen() {
+  var deviceAgent = navigator.userAgent.toLowerCase();
+  var agentID = deviceAgent.match(/(iphone|ipod|ipad)/);
+
+  if (agentID || $(window).width() <= 1020) {
+    $('.videos_home_sources').remove();
+    mainVideo1.append("<source class='videos_home_sources' type='video/mp4' src='./assets/videos/home_1_mobile.mp4' />");
+    mainVideo2.append("<source class='videos_home_sources' type='video/mp4' src='./assets/videos/home_2_mobile.mp4' />");
+    mainVideo3.append("<source class='videos_home_sources' type='video/mp4' src='./assets/videos/home_3_mobile.mp4' />");
+  } else {
+    $('.videos_home_sources').remove();
+    mainVideo1.append("<source class='videos_home_sources' type='video/mp4' src='./assets/videos/home_1.mp4' />");
+    mainVideo2.append("<source class='videos_home_sources' type='video/mp4' src='./assets/videos/home_2.mp4' />");
+    mainVideo3.append("<source class='videos_home_sources' type='video/mp4' src='./assets/videos/home_3.mp4' />");
+  }
 }
